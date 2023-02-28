137ce4458a7db5eea0b9cc4e63ae397b6766142a
ally4n <ali.zeyrek006@gmail.com>
2023-02-28 21:49:25 +0300

    f

--- efficientnet_sample.py
+++ efficientnet_sample.py
@@ -132,7 +132,7 @@ def train_model(model_ft, criterion, optimizer, lr_scheduler, num_epochs=50):
 
         dset_loaders, dset_sizes = loaddata(data_dir=data_dir, batch_size=test_batch_size, set_name='test', shuffle=False)
 
-        print("\nVal starting...")
+        print("\nVal starting...\n")
 
         for data in dset_loaders['test']:
             inputs, labels = data
@@ -155,8 +155,6 @@ def train_model(model_ft, criterion, optimizer, lr_scheduler, num_epochs=50):
         val_acc = running_corrects.double() / dset_sizes
 
         print('Val Loss: {:.4f} Val Acc: {:.4f}'.format(val_loss, val_acc))
-
-        print("Val finished.\n")
 
         write_to_file(val_dir, epoch, val_loss, val_acc)
 
@@ -234,7 +232,7 @@ def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=10):
 
 def write_to_file(path, epoch, loss, acc):
 
-    print(f"\nsaving loss to file: {path}...\n")
+    print(f"\nsaving loss to file: {path}...")
 
     f = open(path, "a+")
 
