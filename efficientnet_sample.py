b96bff9beb7987d184e8e5dcb24ea8c4c777b851
ally4n <ali.zeyrek006@gmail.com>
2023-02-28 19:46:03 +0300

    Update efficientnet_sample.py

--- efficientnet_sample.py
+++ efficientnet_sample.py
@@ -234,7 +234,7 @@ def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=10):
 
 def write_to_file(path, num):
     print(f"writing to file: {path}... "  )
-    f = open(path, ""a+)
+    f = open(path, "a+")
     f.write(num + "\n")
     f.close()
     print("write successful!...\n")
